658798a5e628b435723e66ff9ac38c85ac51c223
ChaseManseth <chasemanseth@gmail.com>
2018-05-23 23:39:44 -0600

    Added Lazy loading

--- js/site.js
+++ js/site.js
@@ -1,3 +1,13 @@
+// Lazy Loading
+window.addEventListener('load', function () {
+    var allimages = document.getElementsByTagName('img');
+    for (var i = 0; i < allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false);
+
 // Home page
 var list = ["VR", "a Team", "the future"];
 var index = 1;
