d59810a523f9e85fe4bd9816bc392f74a29eab84
ChaseManseth <chasemanseth@gmail.com>
2018-05-27 21:24:34 -0600

    Revert "Made a mistake, going to revert"
    This reverts commit 34cce1cc7204177d3b8e476b2693d4aeb57763a4.

--- js/site.js
+++ js/site.js
@@ -1,10 +1,12 @@
 // Lazy Loading
-[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-    img.setAttribute('src', img.getAttribute('data-src'));
-    img.onload = function () {
-        img.removeAttribute('data-src');
-    };
-});
+window.addEventListener('load', function () {
+    var allimages = document.getElementsByTagName('img');
+    for (var i = 0; i < allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false);
 
 // Home page
 var list = ["VR", "a Team", "the future"];
