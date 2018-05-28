34cce1cc7204177d3b8e476b2693d4aeb57763a4
ChaseManseth <chasemanseth@gmail.com>
2018-05-27 21:22:54 -0600

    Made a mistake, going to revert

--- js/site.js
+++ js/site.js
@@ -1,12 +1,10 @@
 // Lazy Loading
-window.addEventListener('load', function () {
-    var allimages = document.getElementsByTagName('img');
-    for (var i = 0; i < allimages.length; i++) {
-        if (allimages[i].getAttribute('data-src')) {
-            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
-        }
-    }
-}, false);
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
+});
 
 // Home page
 var list = ["VR", "a Team", "the future"];
