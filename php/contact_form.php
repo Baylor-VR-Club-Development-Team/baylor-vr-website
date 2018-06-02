de9eca7875d136c99085e296735defba0f92eefc
ChaseManseth <chasemanseth@gmail.com>
2018-06-02 00:14:20 -0600

    Added Composer and MailGun trying some things out

--- php/contact_form.php
+++ php/contact_form.php
@@ -1,44 +1,17 @@
 <?php
+require '../vendor/autoload.php';
+use Mailgun\Mailgun;
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+# First, instantiate the SDK with your API credentials
+$mg = Mailgun::create('key-4eacdef0e417d4df705345ff3f0e0f99');
 
-
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "chasemanseth@gmail.com";
-
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
+# Now, compose and send your message.
+# $mg->messages()->send($domain, $params);
+$mg->messages()->send('mg.baylorvrclub.com', [
+  'from'    => 'baylorvrclub@gmail.com',
+  'to'      => 'chasemanseth@gmail.com',
+  'subject' => 'The PHP SDK is awesome!',
+  'text'    => 'It is so simple to send a message.'
+]);
 
 ?>
