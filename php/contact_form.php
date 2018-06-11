ded0faf193e5a1ba9648b4c2fa167bd53f00bfee
ChaseManseth <chasemanseth@gmail.com>
2018-06-11 01:06:53 -0600

    Few variable rename

--- php/contact_form.php
+++ php/contact_form.php
@@ -10,10 +10,10 @@ $mg = Mailgun::create('key-4eacdef0e417d4df705345ff3f0e0f99');
 # Now, compose and send your message. # $mg->messages()->send($domain, $params); 
 $mg->messages()->send('mg.baylorvrclub.com', 
                       [ 'from' => $email, 
-                       'to' => 'chasemanseth@gmail.com', 
+                       'to' => 'baylorvrclub@gmail.com', 
                        'subject' => 'New email from ' . $name, 
                        'text' => $message ]);
-header("Location: http://bvr.manseth.com/?sent=true"); /* Redirect browser */ 
+header("Location: http://baylorvrclub.com/?sent=true"); /* Redirect browser */ 
 exit();
 
 ?>
