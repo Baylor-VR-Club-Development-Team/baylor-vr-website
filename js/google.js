c47c92c29a62b372db700fc634350e5855745c07
Sean Blonien <sean_blonien@baylor.edu>
2019-09-06 18:37:47 -0500

    Moved google tag to new file, moved mail chimp to new file

---
+++ js/google.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-140787281-1');
---
+++ js/mc.js
@@ -0,0 +1,16 @@
+// Mailchimp Javascript
+(function () {
+    window.fnames = [];
+    window.ftypes = [];
+    fnames[0] = 'EMAIL';
+    ftypes[0] = 'email';
+    fnames[1] = 'FNAME';
+    ftypes[1] = 'text';
+    fnames[2] = 'LNAME';
+    ftypes[2] = 'text';
+    fnames[3] = 'ADDRESS';
+    ftypes[3] = 'address';
+    fnames[4] = 'PHONE';
+    ftypes[4] = 'phone';
+}(jQuery));
+const $mcj = jQuery.noConflict(true);
